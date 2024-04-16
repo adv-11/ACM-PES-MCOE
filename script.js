7630c932b402a93721bd5b70b78c45d81a7aae2d
Advait Shinde <105921012+adv-11@users.noreply.github.com>
2024-04-16 14:38:07 +0530

    deploy_2

--- script.js
+++ script.js
@@ -7,42 +7,59 @@ const sizeMap = new Map();
 let isSliderShown = false;
 
 navItems.forEach((navItem) => {
-	navItem.addEventListener("pointerenter", () => {
-		const navRect = sizeMap.get(nav) || nav.getBoundingClientRect();
-		const itemRect = sizeMap.get(navItem) || navItem.getBoundingClientRect();
+  navItem.addEventListener("pointerenter", () => {
+    const navRect = sizeMap.get(nav) || nav.getBoundingClientRect();
+    const itemRect = sizeMap.get(navItem) || navItem.getBoundingClientRect();
 
-		const transitionDuration = isSliderShown ? 300 : 0;
+    const transitionDuration = isSliderShown ? 300 : 0;
 
-		const sliderWidth = navItem.clientWidth;
-		const sliderHeight = navItem.clientHeight;
-		const sliderLeft = itemRect.left - navRect.left;
+    const sliderWidth = navItem.clientWidth;
+    const sliderHeight = navItem.clientHeight;
+    const sliderLeft = itemRect.left - navRect.left;
 
-		navSlider.animate(
-			[
-				{
-					width: `${sliderWidth}px`,
-					height: `${sliderHeight}px`,
-					transform: `translate(${sliderLeft}px, 0px)`
-				}
-			],
-			{
-				duration: transitionDuration,
-				fill: "forwards",
-				easing: "cubic-bezier(0.33, 1, 0.68, 1)"
-			}
-		);
+    navSlider.animate(
+      [
+        {
+          width: `${sliderWidth}px`,
+          height: `${sliderHeight}px`,
+          transform: `translate(${sliderLeft}px, 0px)`,
+        },
+      ],
+      {
+        duration: transitionDuration,
+        fill: "forwards",
+        easing: "cubic-bezier(0.33, 1, 0.68, 1)",
+      }
+    );
 
-		isSliderShown = true;
-		sizeMap.set(navItem, itemRect);
-		sizeMap.set(nav, navRect);
-	});
+    isSliderShown = true;
+    sizeMap.set(navItem, itemRect);
+    sizeMap.set(nav, navRect);
+  });
 });
 
 nav.addEventListener("pointerleave", () => {
-	isSliderShown = false;
+  isSliderShown = false;
 });
 
 window.addEventListener("resize", () => sizeMap.clear());
 
+function showTeam(year) {
+  if (year === "2024") {
+    document.getElementById("team_2024").style.display = "block";
+    document.getElementById("team_2023").style.display = "none";
+  } else if (year === "2023") {
+    document.getElementById("team_2023").style.display = "block";
+    document.getElementById("team_2024").style.display = "none";
+  }
+}
 
-
+function showEvent(year) {
+  if (year === "2024") {
+    document.getElementsById("hf").style.display = "block";
+    document.getElementById("event_2023").style.display = "none";
+  } else if (year === "2023") {
+    document.getElementById("event_2023").style.display = "block";
+    document.getElementsById("hf").style.display = "none";
+  }
+}
